dfe6622a02e0402f151cd204a6d821eba72b001e
nbc7 <90021751+nbc7@users.noreply.github.com>
2021-12-13 15:11:14 -0300

    js

---
+++ javascript/Introdução a Programação com JavaScript/Folha de Pagamento/codigo.js
@@ -0,0 +1,7 @@
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+let total = parseFloat(valor2 * valor3).toFixed(2)
+
+console.log("NUMBER = " + valor1)
+console.log("SALARY = U$ " + total)
